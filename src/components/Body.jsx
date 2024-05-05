5eb8edb6feefdb20a383da571f732e2f6df41ef4
Arpit <arpittiwari9898@gmail.com>
2024-05-05 23:41:57 +0530

    adding infinite scrolling functionality on jobcards section

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from 'react';
 import Card from './Card';
-import "../assets/jobcard.css"
+import "../assets/jobcard.css";
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Body = () => {
-  const [jobs, setJobs] = useState([ ]);
+  const [jobs, setJobs] = useState([]);
+  const [offset, setOffset] = useState(0);
 
-  const fetchData = async () => {
+  const fetchData = async (limit, offset) => {
     const body = JSON.stringify({
-      limit: 10,
-      offset: 0
+      limit: limit,
+      offset: offset
     });
 
     const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", {
@@ -20,19 +22,42 @@ const Body = () => {
     });
 
     const jobData = await response.json();
-    setJobs(jobData.jdList);
+    return jobData.jdList;
+  };
+
+  const fetchMoreData = async () => {
+    const newJobs = await fetchData(12, offset);
+    setJobs([...jobs, ...newJobs]);
+    setOffset(offset + 12); // Increment offset for the next fetch
   };
 
   useEffect(() => {
-    fetchData();
+    fetchData(12, 0).then(initialJobs => { // fetching initial data setting offset to 0 and limit to 12.
+      setJobs(initialJobs);
+      setOffset(12);
+    });
   }, []);
 
-  return (jobs && jobs.length === 0)  ? (<div>Loading ...</div>) : (
+  return (
+    <>
     <div className='cards-wrapper'>
       {jobs.map((job) => (
-        <Card key = {job.jdUid} jobid = {job.jdUid} companyName = {job.companyName} description = {job.jobDetailsFromCompany} jobTitle = {job.jobRole} location ={job.location} minExp = {job.minExp} jdLink = {job.jdLink} logoUrl = {job.logoUrl}/>
+        <Card key={job.jdUid} jobid={job.jdUid} companyName={job.companyName} description={job.jobDetailsFromCompany} jobTitle={job.jobRole} location={job.location} minExp={job.minExp} jdLink={job.jdLink} logoUrl={job.logoUrl} />
       ))}
     </div>
+    <InfiniteScroll  // implemented infinite scroll functionality using react-infinite-scroll-library
+        dataLength={jobs.length} 
+        next={fetchMoreData}
+        hasMore={true}
+        loader={<h4 style={{textAlign: 'center'}}>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+      </InfiniteScroll>
+    </>
   );
 };
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,6 @@ const Card = ({jobid, companyName, description, jobTitle, location, minExp, jdLi
           <div className='companyname'>{companyName}</div>
           <div className='role'>{jobTitle[0].toUpperCase() + jobTitle.slice(1)}</div>
           <div className='location'>{location[0].toUpperCase() + location.slice(1)}</div>
-
         </div>
       </div>
       <div className='description'>{description}</div>
@@ -18,7 +17,9 @@ const Card = ({jobid, companyName, description, jobTitle, location, minExp, jdLi
         <div>Minimum Experience</div>
         <div className='minexp'>{minExp}</div>
       </div>
-      <button className='apply' onClick={()=> {}}>Apply</button>
+      <button className='apply' onClick={()=> {
+        console.log("*********")
+      }}>Apply</button>
     </div>
   )
 }
